ddaa62d2116ee087d1e31207ca56cff7784282bb
guacajoely <guacajoely@gmail.com>
2023-06-01 13:39:33 -0400

    CreativeSmackTalker complete

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
-
-        
-
+        public List<string> Taunts { get; } = new List<string>(){"Taunt 1", "Taunt 2", "Taunt 3", "Taunt 4", "Taunt 5", "Taunt 6", "Taunt 7", "Taunt 8", "Taunt 9", "Taunt 10" };
+        int TauntSelection = new Random().Next(0, 9);
+        public override int Roll()
+        {
+            // Return a random number between 1 and DiceSize
+            Console.WriteLine(Taunts[TauntSelection]);
+            return new Random().Next(DiceSize) + 1;
+        }
     }
 }
--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -8,12 +8,12 @@ namespace ShootingDice
     // A Player who shouts a taunt every time they roll dice
     public class SmackTalkingPlayer : Player
     {
-        public List<string> Taunts { get; } = new List<string>(){"Taunt 1", "Taunt 2", "Taunt 3", "Taunt 4", "Taunt 5", "Taunt 6", "Taunt 7", "Taunt 8", "Taunt 9", "Taunt 10" };
-        int TauntSelection = new Random().Next(0, 9);
+        public string Taunt { get; } = "SCARED MONEY DONT MAKE MONEY";
+
         public override int Roll()
         {
             // Return a random number between 1 and DiceSize
-            Console.WriteLine(Taunts[TauntSelection]);
+            Console.WriteLine(Taunt);
             return new Random().Next(DiceSize) + 1;
         }
     }
