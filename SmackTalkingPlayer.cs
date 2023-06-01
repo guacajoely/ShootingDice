b66081047d85f7ff06d794a15567dd5ce4d3cb9f
guacajoely <guacajoely@gmail.com>
2023-06-01 11:30:20 -0400

    OneHigherPlayer complete

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -10,14 +10,11 @@ namespace ShootingDice
     {
         public List<string> Taunts { get; } = new List<string>(){"Taunt 1", "Taunt 2", "Taunt 3", "Taunt 4", "Taunt 5", "Taunt 6", "Taunt 7", "Taunt 8", "Taunt 9", "Taunt 10" };
         int TauntSelection = new Random().Next(0, 9);
-
         public override int Roll()
         {
             // Return a random number between 1 and DiceSize
             Console.WriteLine(Taunts[TauntSelection]);
             return new Random().Next(DiceSize) + 1;
-
-
         }
     }
 }
